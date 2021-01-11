143ace3ba612dfd90e86309d048b386ce8d1cbc3
Anya Sendzyak <sendzyak2017@gmail.com>
2021-01-11 22:31:33 +0700

    fix News model

--- src/model/News.kt
+++ src/model/News.kt
@@ -1,12 +1,11 @@
 package model;
 
-import io.ktor.http.ContentType
 import java.io.Serializable
 
 data class News(
     val title: String,
     val content: String,
     val date: String,
-    val image: ContentType.Image? = null
+    val author: String? = null
 ) : Serializable
 
